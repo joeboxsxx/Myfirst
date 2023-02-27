7750319e1d9b4e23c7473028b4e6aa5192cb583f
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-02-27 14:35:40 +0900

    Initial Commit

---
+++ Myfirst/MyfirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstApp.swift
+//  Myfirst
+//
+//  Created by 戸上健太 on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
